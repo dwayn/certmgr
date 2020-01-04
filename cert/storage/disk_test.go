555f1fb925eb28fd7b062163aac549219bc075ae
Brian Harring <brian@cloudflare.com>
2020-01-04 12:30:51 -0800

    Expand tests of FileBackend

--- cert/storage/disk_test.go
+++ cert/storage/disk_test.go
@@ -1,39 +1,85 @@
 package storage
 
 import (
+	"io/ioutil"
+	"os"
+	"reflect"
 	"sort"
 	"testing"
 
 	"github.com/cloudflare/certmgr/file"
 )
 
-func TestFileBackendPaths(t *testing.T) {
-	assert := func(desired, received []string) {
-		sort.Strings(desired)
-		sort.Strings(received)
-
-		if len(received) != len(desired) {
-			t.Fatalf("%s != %s", desired, received)
-		}
-		for idx := range received {
-			if desired[idx] != received[idx] {
-				t.Fatalf("%s != %s", desired, received)
-			}
-		}
+func getFilesAndPaths(ca, cert, key string) (caFile *file.CertificateFile, certFile *file.CertificateFile, keyFile *file.File, paths []string) {
+	if ca != "" {
+		caFile = &file.CertificateFile{File: file.File{Path: ca}}
+		paths = append(paths, ca)
 	}
+	if cert != "" {
+		certFile = &file.CertificateFile{File: file.File{Path: cert}}
+		paths = append(paths, cert)
+	}
+	if key != "" {
+		keyFile = &file.File{Path: key}
+		paths = append(paths, key)
+	}
+	return
+}
 
-	fb := FileBackend{}
+func TestFileBackend(t *testing.T) {
+	tests := []struct {
+		name string
+		ca   string
+		cert string
+		key  string
+		fail bool
+	}{
+		{"just ca", "/ca", "", "", false},
+		{"just certs", "", "/cert", "/key", false},
+		{"all", "/ca", "/cert", "/key", false},
+		{"missing key", "", "/cert", "", true},
+		{"missing cert", "", "", "/key", true},
+		{"all missing", "", "", "", true},
+		{"reused path1", "", "/reused", "/reused", true},
+		{"reused path2", "/reused", "/reused", "/path2", true},
+		{"reused path3", "/reused", "/path2", "/reused", true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ca, cert, key, expectedPaths := getFilesAndPaths(test.ca, test.cert, test.key)
 
-	// ensure that an empty spec doesn't trigger a panic
-	assert([]string{}, fb.GetPaths())
-	fb.ca = &file.CertificateFile{file.File{Path: "/ca"}}
+			fb, err := NewFileBackend(ca, cert, key)
+			if test.fail {
+				if err == nil {
+					t.Fatal("Error wasn't returned")
+				}
+				return
+			} else if err != nil {
+				t.Fatalf("unexpected err returned from init: %s", err)
+			}
+			if (test.key != "") != fb.WantsKeyPair() {
+				t.Fatalf("WantsKeyPair returned %t, expects %t", fb.WantsKeyPair(), test.key != "")
+			}
 
-	assert([]string{"/ca"}, fb.GetPaths())
-	fb.cert = &file.CertificateFile{file.File{Path: "/cert"}}
-	fb.key = &file.File{Path: "/key"}
-
-	assert([]string{"/ca", "/key", "/cert"}, fb.GetPaths())
-
-	fb.ca = nil
-	assert([]string{"/key", "/cert"}, fb.GetPaths())
+			sort.Strings(expectedPaths)
+			returnedPaths := fb.GetPaths()
+			sort.Strings(returnedPaths)
+			if !reflect.DeepEqual(expectedPaths, returnedPaths) {
+				t.Fatalf("wanted %v from GetPaths, got %v", expectedPaths, returnedPaths)
+			}
+			tmpDir, err := ioutil.TempDir("", "tests")
+			if err != nil {
+				t.Fatalf("falied to create tempdir: %s", err)
+			}
+			defer os.RemoveAll(tmpDir)
+			existingCA, existingKeyPair, err := fb.Load()
+			if existingCA != nil || existingKeyPair != nil {
+				t.Fatalf("initial Load() returned non-nill CA=%v keyPair=%v", existingCA, existingKeyPair)
+			}
+			if err == nil {
+				t.Fatalf("didn't receive err from initial empty Load")
+			}
+			// todo; write actual tests for generating PKI and verifying the backend persists/returns it properly.
+		})
+	}
 }
